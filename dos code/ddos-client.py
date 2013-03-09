c2dfa26a6a8c51cede6394b1faa71aed9100ca18
Mohammad Reza Heidarian <mrhsce@gmail.com>
2013-03-09 16:09:55 +0330

    DDOS client

--- dos code/ddos-client.py
+++ dos code/ddos-client.py
@@ -9,6 +9,24 @@
 ##                        Written in python 3.3
 '''Program number: 59 Date: 9/3/2013 DtD: 1065 Copyright:(c)MrHs 2013'''
 
+import socket
+
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#*****Constants
+host=""
+port=1373
+address=socket.getaddrinfo(host,port)
+#*****/Constants
+
+
+client.connect(address)
+
+
+
+if client.recv(2**10):
+    pass
+    #Here should be the pinging application
 
 
 
